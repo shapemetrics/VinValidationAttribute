0414cbbc6112fe603ad165fea0ae41a4725187e1
user <user@BLUEROOFIES>
2017-08-07 01:38:35 -0400

    Master Set

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("shapemetrics.VinValidation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("shapemetrics.VinValidation")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb64b68d-eb4a-4bcc-9ba0-deb998350369")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VinValidationAttribute.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Collections;
+
+
+namespace shapemetrics.VinValidation
+{
+
+
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class ValidBirthDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            string p_strVin = "";
+            if (value != null)
+            {
+                p_strVin = (string)value;
+            }
+
+
+            int intValue = 0;
+            //Multiplier weights for each position.
+            int[] intWeights = { 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+
+            if (string.IsNullOrEmpty(p_strVin) || p_strVin.Length != 17)
+            {
+                return new ValidationResult(String.Format("Invalid length for {0}", validationContext.DisplayName));
+            }
+
+            //Capitalize all values in VIN
+            p_strVin = p_strVin.ToUpper().Trim();
+            int intCheckValue = 0;
+            //Get the Check digit from VIN
+            char check = p_strVin[8];
+            //Get the Year from the VIN
+            char year = p_strVin[9];
+            //Ensure the check digit is 0-9 or X
+            if (!char.IsDigit(check) && check != 'X')
+            {
+                return new ValidationResult(String.Format("Check Digit is invalid {0}", validationContext.DisplayName));
+            }
+            else
+            {
+                //Get the numeric value of the check digit
+                if (check != 'X')
+                {
+                    char[] d = new char[] { check };
+                    intCheckValue = int.Parse(Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(d)));
+                }
+                else
+                {
+                    intCheckValue = 10;
+                }
+            }
+            //Hash table of the character replacement values
+            Hashtable replaceValues = new Hashtable();
+            replaceValues.Add('A', 1);
+            replaceValues.Add('B', 2);
+            replaceValues.Add('C', 3);
+            replaceValues.Add('D', 4);
+            replaceValues.Add('E', 5);
+            replaceValues.Add('F', 6);
+            replaceValues.Add('G', 7);
+            replaceValues.Add('H', 8);
+            replaceValues.Add('J', 1);
+            replaceValues.Add('K', 2);
+            replaceValues.Add('L', 3);
+            replaceValues.Add('M', 4);
+            replaceValues.Add('N', 5);
+            replaceValues.Add('P', 7);
+            replaceValues.Add('R', 9);
+            replaceValues.Add('S', 2);
+            replaceValues.Add('T', 3);
+            replaceValues.Add('U', 4);
+            replaceValues.Add('V', 5);
+            replaceValues.Add('W', 6);
+            replaceValues.Add('X', 7);
+            replaceValues.Add('Y', 8);
+            replaceValues.Add('Z', 9);
+            replaceValues.Add('1', 1);
+            replaceValues.Add('2', 2);
+            replaceValues.Add('3', 3);
+            replaceValues.Add('4', 4);
+            replaceValues.Add('5', 5);
+            replaceValues.Add('6', 6);
+            replaceValues.Add('7', 7);
+            replaceValues.Add('8', 8);
+            replaceValues.Add('9', 9);
+            replaceValues.Add('0', 0);
+
+            //Make sure it is a Valid Year 
+            if (!replaceValues.Contains(year) && year != '0')
+            {
+                return new ValidationResult(String.Format("Year is invalid {0}", validationContext.DisplayName));
+            }
+
+            //Make sure characters valid values. 
+            for (int i = 0; i < p_strVin.Length; i++)
+            {
+                if (!replaceValues.Contains(p_strVin[i]))
+                {
+                    return new ValidationResult(String.Format("Invalid Character for {0} at position {1}", validationContext.DisplayName, i + 1)); ;
+                }
+                intValue += (intWeights[i] * ((int)replaceValues[p_strVin[i]]));
+            }
+
+
+            if ((intValue % 11) == intCheckValue)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(String.Format("Unknown error for {0}", validationContext.DisplayName)); ;
+        }
+    }
+
+}
